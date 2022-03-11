e944825860679a9df15cfc49e36b53b8b3743537
seohui Yang <seoulyon@gmail.com>
2022-03-11 17:18:59 +0900

    단계별로 풀어보기 - 1단계 입출력과 사칙연산

---
+++ Algorithm/src/baekjoon/boj10430.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj10430 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int r1 = (a+b)%c;
+		int r2 = ((a%c)+(b%c))%c;
+		int r3 = (a*b)%c;
+		int r4 = ((a%c)*(b%c))%c;
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
+	}
+
+}
