e26f62cc8e1bef1618f439e1c0e95f4bc403af5a
seohui Yang <seoulyon@gmail.com>
2022-03-11 15:55:07 +0900

    단계별로 풀어보기 - 1단계 입출력과 사칙연산

---
+++ Algorithm/src/baekjoon/boj10171.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+public class boj10171 {
+	public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+}
