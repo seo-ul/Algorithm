fea0f7a375e6de6e2263dcde108d780c094d432a
seohui Yang <seoulyon@gmail.com>
2022-03-14 15:45:08 +0900

    단계별로 풀어보기 - 2단계 if문

---
+++ Algorithm/src/baekjoon/boj2480.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj2480 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[] = br.readLine().split(" ");
+		int a = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
+		int c = Integer.parseInt(str[2]);
+		
+		Arrays.sort(str);
+		
+		if (a == b && b == c)
+			System.out.println(10000 + (a * 1000));
+		else if (a != b && b != c && a != c)
+			System.out.println(Integer.parseInt(str[2]) * 100);
+		else {
+			if (a == b && a == c)
+				System.out.println(1000 + (a * 100));
+			else if (b == c)
+				System.out.println(1000 + (b * 100));
+		}
+	}
+
+}
