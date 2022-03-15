df70c4fb57c584f59d358ebd28d90a4c187af418
seohui Yang <seoulyon@gmail.com>
2022-03-15 10:34:40 +0900

    단계별로 풀어보기 - 3단계 for문

---
+++ Algorithm/src/baekjoon/boj2741.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj2741 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		
+		for (int i = 1; i <= n; i++) {
+			bw.write(i+"\n");
+		}
+		bw.close();
+	}
+
+}
