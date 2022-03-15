aa7a60296c80a9176036dc1898bc6983a3f63683
seohui Yang <seoulyon@gmail.com>
2022-03-15 10:26:31 +0900

    단계별로 풀어보기 - 3단계 for문

---
+++ Algorithm/src/baekjoon/boj15552.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj15552 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i=0; i<T; i++) {
+			st=new StringTokenizer(br.readLine()," ");
+			sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()));
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
