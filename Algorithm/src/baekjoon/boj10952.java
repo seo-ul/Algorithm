c7d51aa25c7d664019460e77f19fa5c26864ff53
seohui Yang <seoulyon@gmail.com>
2022-03-16 09:43:54 +0900

    단계별로 풀어보기 - 4단계 while문

--- Algorithm/src/baekjoon/boj10952.java
+++ Algorithm/src/baekjoon/boj10952.java
@@ -9,40 +9,39 @@ public class boj10952 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
-		while(true){
-			st = new StringTokenizer(br.readLine()," ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			if(a==0 && b==0) {
+
+		while (true) {
+			String str = br.readLine();
+			int a = str.charAt(0) - '0'; // 10 미만이라서 가능한
+			int b = str.charAt(2) - '0';
+
+			if (a == 0 && b == 0) {
 				break;
 			}
-			sb.append(a+b).append('\n');
+			sb.append(a + b).append('\n');
 		}
 		br.close();
 		System.out.println(sb);
-		
 	}
 }
 
-//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//StringBuilder sb = new StringBuilder();
-//
-//while (true) {
-//	String str = br.readLine();
-//	int a = str.charAt(0) - '0'; // 10 미만이라서 가능한
-//	int b = str.charAt(2) - '0';
-//
-//	if (a == 0 && b == 0) {
-//		break;
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st;
+//		StringBuilder sb = new StringBuilder();
+//		
+//		while(true){
+//			st = new StringTokenizer(br.readLine()," ");
+//			int a = Integer.parseInt(st.nextToken());
+//			int b = Integer.parseInt(st.nextToken());
+//			
+//			if(a==0 && b==0) {
+//				break;
+//			}
+//			sb.append(a+b).append('\n');
+//		}
+//		br.close();
+//		System.out.println(sb);
+//		
 //	}
-//	sb.append(a + b).append('\n');
-//}
-//br.close();
-//System.out.println(sb);
-//}
 //}
